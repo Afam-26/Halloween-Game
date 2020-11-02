a9cbde9a0d7e8e8366e72645814a49f16bc8d950
Afam <afam262@gmail.com>
2020-11-02 01:29:29 -0600

    adding var in js and linking it to html

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+var timeEl = document.querySelector(".time");
+var mainEl = document.querySelector("#main");
+var nextButton = document.querySelector("#next");
+var clearHighScore = document.querySelector("#clear-hgh");
 
+var secondsLeft = 10
+
+function setTime() {
+    var timerInterval = setInterval(function() { 
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval)
+            return
+        }
+        
+    }, 1000);
+    
+}
+
+setTime();
